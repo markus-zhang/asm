e91f3950cb8108ada68b1fe90012526bbd7684b7
markus-zhang <markus_zhang@hotmail.com>
2025-01-03 04:21:02 +0000

    Added more tests for ADD, LD, ST, JSR, AND

--- jit/lc3disa.c
+++ jit/lc3disa.c
@@ -16,20 +16,7 @@ void dis_br(uint16_t instr, uint16_t address)
 		0  0  0  0  | n  z  p |    PCOffset9
 	*/
 
-	// First we print the address
-	printf("Address: %#06x\n", (unsigned int)address);
-
-	if (DEBUG_LEVEL == 2)
-	{
-		// Show current instruction in hex and mnenomics
-		printf("Instruction to be executed - %#06x\n", instr);
-		printf("15 14 13 12 11 10 09 08 07 06 05 04 03 02 01 00\n");
-		for (int i = 15; i >= 0; i--)
-		{
-			printf("%hhu  ", (instr >> i) & 0x01);
-		}
-		printf("\nPress ENTER to execute the instruction.\n");
-	}
+	dis_debug(instr, address);
 
 	printf("BR");
 
@@ -42,7 +29,109 @@ void dis_br(uint16_t instr, uint16_t address)
 	if (p) {putchar('p');}
 
 	putchar('\t');
-	printf("%#06x\n", (instr & 0x01FF));
+	printf("%#06x\n", sign_extended(instr & 0x01FF, 9));
+}
+
+void dis_add(uint16_t instr, uint16_t address)
+{
+	/* 
+		15 14 13 12 | 11 10 9 | 8 7 6 | 5 | 4 3 | 2 1 0
+		0  0  0  1  |   DR    |  SR1  | 0 | 0 0 |  SR2 
+		----------------------or-----------------------
+		15 14 13 12 | 11 10 9 | 8 7 6 | 5 | 4 3 2 1 0
+		0  0  0  1  |   DR    |  SR   | 1 |    IMM
+	*/
+
+	dis_debug(instr, address);
+
+	printf("ADD");
+
+	putchar('\t');
+
+	uint8_t dr = (instr >> 9) & 0x0007;
+	printf("r%hhu\t", dr);
+	uint8_t sr = (instr >> 6) & 0x0007;
+	printf("r%hhu\t", sr);
+
+	uint8_t mode = (instr >> 5) & 0x0001;
+
+	if (mode)
+	{
+		printf("%#06x\n", sign_extended(instr & 0x001F, 5));
+	}
+	else 
+	{
+		uint8_t sr2 = instr & 0x0007;
+		printf("r%hhu\n", sr2);
+	}
+}
+
+
+void dis_ld(uint16_t instr, uint16_t address)
+{
+	/* 
+		15 14 13 12 | 11 10 9 | 8 7 6 5 4 3 2 1 0
+		0  0  1  0  |   DR    |    PCOffset9
+	*/
+
+	dis_debug(instr, address);
+
+	printf("LD");
+
+	putchar('\t');
+
+	uint8_t dr = (instr >> 9) & 0x0007;
+	printf("r%hhu\t", dr);
+
+	printf("%#06x\n", sign_extended(instr & 0x01FF, 9));
+}
+
+void dis_st(uint16_t instr, uint16_t address)
+{
+	/* 
+		15 14 13 12 | 11 10 9 | 8 7 6 5 4 3 2 1 0
+		0  0  1  1  |   SR    |    PCOffset9
+	*/
+
+	dis_debug(instr, address);
+
+	printf("ST");
+
+	putchar('\t');
+
+	uint8_t sr = (instr >> 9) & 0x0007;
+	printf("r%hhu\t", sr);
+
+	printf("%#06x\n", sign_extended(instr & 0x01FF, 9));
+}
+
+void dis_jsr(uint16_t instr, uint16_t address)
+{
+	/* 
+		15 14 13 12 | 11 | 10 9 8 7 6 5 4 3 2 1 0
+		0  1  0  0  | 1  |      PCOffset11
+		-----------------or----------------------
+		15 14 13 12 | 11 | 10 9 | 8 7 6 | 5 4 3 2 1 0
+		0  1  0  0  | 0  | 0  0 |   BR  | 0 0 0 0 0 0
+	*/
+
+	dis_debug(instr, address);
+
+	printf("JSR");
+
+	putchar('\t');
+
+	uint8_t mode = (instr >> 11) & 0x0001;
+
+	if (mode)
+	{
+		printf("%#06x\n", sign_extended(instr & 0x07FF, 11));
+	}
+	else 
+	{
+		uint16_t br = (instr >> 6)  & 0x0007;
+		printf("r%hhu\n", br);
+	}
 }
 
 void dis_and(uint16_t instr, uint16_t address)
@@ -55,20 +144,7 @@ void dis_and(uint16_t instr, uint16_t address)
 		0  1  0  1  |    DR   |  SR1  | 1 |   imm5
 	*/
 
-	// First we print the address
-	printf("Address: %#06x\n", (unsigned int)address);
-
-	if (DEBUG_LEVEL == 2)
-	{
-		// Show current instruction in hex and mnenomics
-		printf("Instruction to be executed - %#06x\n", instr);
-		printf("15 14 13 12 11 10 09 08 07 06 05 04 03 02 01 00\n");
-		for (int i = 15; i >= 0; i--)
-		{
-			printf("%hhu  ", (instr >> i) & 0x01);
-		}
-		printf("\nPress ENTER to execute the instruction.\n");
-	}
+	dis_debug(instr, address);
 
 	printf("AND");
 
@@ -83,13 +159,35 @@ void dis_and(uint16_t instr, uint16_t address)
 	if (mode)
 	{
 		uint16_t imm5 = sign_extended(instr & 0x001F, 5);
-		printf("%hhu\n", imm5);
+		printf("%#06x\n", imm5);
 	}
 	else
 	{
 		uint8_t sr2 = instr & 0x0007;
 		printf("r%hhu\n", sr2);
 	}
+}
+
+
+
+void dis_debug(uint16_t instr, uint16_t address)
+{
+
+	if (DEBUG_LEVEL == 2)
+	{
+		// Show current instruction in hex and mnenomics
+		printf("Instruction to be executed - %#06x\n", instr);
+		printf("15 14 13 12 11 10 09 08 07 06 05 04 03 02 01 00\n");
+		for (int i = 15; i >= 0; i--)
+		{
+			printf("%hhu  ", (instr >> i) & 0x01);
+		}
+		printf("\n");
+	}
+
+	// First we print the address
+	printf("Address: %#06x\t", (unsigned int)address);
+	printf("%#06x\t", instr);
 }
 
 uint16_t sign_extended(uint16_t num, uint8_t effBits)
--- jit/lc3disa.h
+++ jit/lc3disa.h
@@ -5,11 +5,16 @@
 #include <unistd.h>
 #include <stdint.h>
 
-#define DEBUG_LEVEL 2
+#define DEBUG_LEVEL 1
 
 uint16_t sign_extended(uint16_t num, uint8_t effBits);
 
+void dis_debug(uint16_t instr, uint16_t address);
 void dis_br(uint16_t instr, uint16_t address);
+void dis_add(uint16_t instr, uint16_t address);
+void dis_ld(uint16_t instr, uint16_t address);
+void dis_st(uint16_t instr, uint16_t address);
+void dis_jsr(uint16_t instr, uint16_t address);
 void dis_and(uint16_t instr, uint16_t address);
 
 #endif
